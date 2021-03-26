df8e84c05a35c67be4362e4b3e980b3aae4f3277
Anthony10700 <anthony.thillerot@gmail.com>
2021-03-26 14:37:08 +0100

    add info heroku

--- free_chess_gui/free_chess_gui/settings.py
+++ free_chess_gui/free_chess_gui/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -23,12 +24,15 @@ print(BASE_ALL_APP_PATH)
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^e!tl*(m!30piznn9fy$$hfqbbhhg9h-&6i0u6r7h=345+)%x('
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    '^e!tl*(m!30piznn9fy$$hfqbbhhg9h-&6i0u6r7h=345+)%x(')
+if os.environ.get('ENV') == 'PRODUCTION':
+    DEBUG = False
+else:
+    DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://chess-app-gui.herokuapp.com/']
 
 
 # Application definition
@@ -52,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'free_chess_gui.urls'
@@ -163,5 +168,17 @@ CHAT_WS_SERVER_PORT = 5002
 CHAT_WS_SERVER_PROTOCOL = 'ws'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+
+    STATICFILES_STORAGE = (
+        'whitenoise.storage.CompressedManifestStaticFilesStorage')
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
