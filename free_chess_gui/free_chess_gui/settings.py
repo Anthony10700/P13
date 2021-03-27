e86fc1dcacb7039ef59487a2eaf8cbc20c675aaa
Anthony10700 <anthony.thillerot@gmail.com>
2021-03-27 12:53:50 +0100

    11

--- free_chess_gui/free_chess_gui/settings.py
+++ free_chess_gui/free_chess_gui/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'chess_app/static'),
                     os.path.join(BASE_DIR, 'auth/static'),
